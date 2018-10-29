f01a4f3bde0c7755161bdc7454bab79851078ff7
kimbouvet <kim.oftedal@bouvet.no>
2018-10-29 16:03:15 +0100

    Update career profile

--- src/data/careerProfile.js
+++ src/data/careerProfile.js
@@ -1,7 +1,7 @@
 export const careerProfile = {
   sectionTitle: 'Career Profile',
   icon: null,
-  description: '<p>Full Stack Software Developer from Stavanger, Norway.</p><p>Graduated from the Computer Science course at London Metropolitan University with honours in june 2012.</p><p>A lot of experience in modern web development (.net, JS frameworks including React, Angular and REST APIs)</p>'
+  description: '<p>Full Stack Software Developer from Stavanger, Norway.</p><p>A lot of experience in modern web and mobile development</p>'
 };
 
 export default careerProfile;
